3e38f44c87ca54231a5ae0d1354e8db7f61c488a
breezekaga <briankang528@gmail.com>
2019-09-23 20:11:04 +0900

    LE198 Predicting Projectile Arc

--- BreezeTank/Source/BreezeTank/Public/Tank.h
+++ BreezeTank/Source/BreezeTank/Public/Tank.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "TankAimingComponent.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
 
@@ -18,9 +19,13 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	UTankAimingComponent* TankAimingComponent = nullptr;
 
 public:
 	void AimAt(FVector HitLocation);
+
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
 
 private:	
 	// Called every frame
@@ -29,4 +34,6 @@ private:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(EditAnywhere, Category = Firing)
+	float LaunchSpeed = 100000;
 };
